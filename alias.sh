ae6ea62748ffa6d2dc2eed8c61206b4fdff1fc9e
TrinkYet <kejunbo@hotmail.com>
2016-11-05 16:33:42 -0700

    restart_storage function added

--- alias.sh
+++ alias.sh
@@ -1,5 +1,11 @@
 WEBFRONT_CONTAINER=h_webfront
 CACHE_CONTAINER=h_cache
+STORAGE_CONTAINER=h_storage
+STORAGE_IP=172.20.0.6
+STORAGE_SERVER_PORT=8081
+STORAGE_IMAGE=hyoung/haystack_storage
+STORAGE_INTERNAL_PORT=8080
+LOCAL_STORAGE_DIR=$(pwd)'/storage'
 NETWORK=haystack_network
 WEBFRONT_IP=172.20.0.3
 WEBFRONT_IMAGE=hyoung/haystack_webfront
@@ -28,6 +34,18 @@ function restart_webfront() {
     --network $NETWORK \
     --ip $WEBFRONT_IP \
     $WEBFRONT_IMAGE
+}
+
+function restart_storage() {
+  docker stop $STORAGE_CONTAINER
+  docker rm $STORAGE_CONTAINER
+  docker build -t $STORAGE_IMAGE $LOCAL_STORAGE_DIR
+  docker run -itd \
+  --name $STORAGE_CONTAINER \
+  --network $NETWORK \
+  --ip $STORAGE_IP \
+  -p $STORAGE_SERVER_PORT:$STORAGE_INTERNAL_PORT \
+  $STORAGE_IMAGE
 }
 
 # docker logs h_webfront  # check container log
