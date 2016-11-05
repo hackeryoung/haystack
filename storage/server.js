fbac90df3ed7f2bef20a7c9f42ddd7d630ad176e
kunhuang <kunhuang110@126.com>
2016-11-05 16:39:39 -0700

    READ request

--- storage/server.js
+++ storage/server.js
@@ -5,6 +5,12 @@ const responseTime = require('response-time');
 const redis = require('redis');
 const fs = require('fs');
 
+// create a new redis client and connect to our local redis instance
+var client = redis.createClient();
+
+// Set key value pair: [photoid, [offset, size, type]]
+// client.set('1', ['0', '1024', 'jpg'])
+
 // set the server listening port
 app.set('port', (process.env.PORT || 8080));
 
@@ -12,8 +18,30 @@ app.set('port', (process.env.PORT || 8080));
 app.use(responseTime());
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+  res.send('Hello World');
+});
+
+// READ request
+app.get('/:lvid/:photoid', function(req, res) {
+  var lvid = req.params.lvid
+  var photoid = req.params.photoid;
+
+  console.log('Received READ request:');
+  console.log('logical volumn id: '+lvid);
+  console.log('photo id: '+photoid);
+
+  // client.get(photoid, function(err, reply) {
+  //   console.log(reply);
+  // })
+
+  res.send('OK')
+});
+
+// WRITE request
+// TODO
+
+// DELETE request
+// TODO
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port: ', app.get('port'));
