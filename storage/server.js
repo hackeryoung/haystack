433c56d35bdd990a0775b93f8e3a04369a06c4b7
kunhuang <kunhuang110@126.com>
2016-11-10 20:03:54 -0800

    DELETE API

--- storage/server.js
+++ storage/server.js
@@ -12,8 +12,8 @@ const upload = multer({ storage: storage }).single('image');
 // create a new redis client and connect to our local redis instance
 var client = redis.createClient();
 
-// Set key value pair: [photoid, [offset, size, type]]
-client.rpush(['1', '0', '82931', 'gif']);
+// Set key value pair: [photoid, [offset, size, type, is_exist]]
+client.rpush(['1', '0', '82931', 'gif', true]);
 
 // set the server listening port
 app.set('port', (process.env.PORT || 8080));
@@ -37,6 +37,11 @@ app.get('/:lvid/:photoid', function(req, res) {
   client.lrange(photoid, 0, -1, function(err, reply) {
     if (err){
       const msg = 'In-memory mapping fails';
+      console.error(msg, err);
+      res.status(400);
+      res.send(msg);
+    } else if (reply[3] == 'false') {
+      const msg = 'Data deleted';
       console.error(msg, err);
       res.status(400);
       res.send(msg);
@@ -91,7 +96,7 @@ app.post('/:lvid/:photoid/:type', function(req, res) {
         res.send('something is wrong');
         process.exit(1);
       } else {
-        client.rpush([photoid, offset, size, type]);
+        client.rpush([photoid, offset, size, type, true]);
         res.send("OK");
       }
     });
@@ -102,7 +107,42 @@ app.post('/:lvid/:photoid/:type', function(req, res) {
 });
 
 // DELETE request
-// TODO
+app.delete('/:lvid/:photoid', function(req, res) {
+  var lvid = req.params.lvid;
+  var photoid = req.params.photoid;
+
+  console.log('Received DELETE request:');
+  console.log('logical volumn id: '+lvid);
+  console.log('photo id: '+photoid);
+
+  client.lrange(photoid, 0, -1, function(err, reply) {
+    if (err){
+      const msg = 'In-memory mapping fails';
+      console.error(msg, err);
+      res.status(400);
+      res.send(msg);
+    } else if (reply[3] == 'false') {
+      const msg = 'Already deleted';
+      console.error(msg, err);
+      res.status(400);
+      res.send(msg);
+    } else {
+      // reset it as false
+      client.del(photoid, function(err, _) {
+        if (err) {
+          const msg = 'Unknown error';
+          console.error(msg, err);
+          res.status(400);
+          res.send(msg);
+        } else {
+          client.rpush([photoid, reply[0], reply[1], reply[2], false]);
+          res.status(200);
+          res.send('OK');
+        }
+      });
+    }
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server listening on port: ', app.get('port'));
